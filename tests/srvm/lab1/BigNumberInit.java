ed214cad3ac0b6fd62b52564231799d3aaf0b1e5
librt <geor.polina@gmail.co>
2013-12-12 21:27:01 +0200

    Добавлены тесты

--- tests/srvm/lab1/BigNumberInit.java
+++ tests/srvm/lab1/BigNumberInit.java
@@ -2,9 +2,28 @@ package srvm.lab1;
 
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Pending;
 import org.jbehave.core.annotations.Then;
-import org.jbehave.core.annotations.When;
-import org.jbehave.core.steps.Steps;
 
-public class BigNumberInit extends Steps {
+public class BigNumberInit {
+    BigNumber a;
+
+    @Given("a big number with size $size")
+    public void givenBigNumberWithSize(@Named("size") int size) {
+        a = new BigNumber(size);
+    }
+
+    @Then("big number size should be $size")
+    public void thenSizeShouldBe(@Named("size") int size) {
+        if (a.size() != size)
+            throw new RuntimeException("size is " + a.size() + ", but should be " + size);
+    }
+
+    @Then("big number should be $number")
+    @Pending
+    public void thenBigNumberShouldBe(@Named("number") String number) {
+        if (!a.toString().equals(number))
+            throw new RuntimeException("big number is " + a.toString() + ", but should be " + number);
+    }
+
 }
---
+++ tests/srvm/lab1/big_number_init.story
@@ -0,0 +1,11 @@
+Meta:
+
+Narrative:
+As a user
+I want to perform an action
+So that I can achieve a business goal
+
+Scenario: blank BigNum initialization
+Given a big number with size 10
+Then big number size should be 10
+Then big number should be 0
