fc1c023bdd45d538c4fd7990cd801c196a1fec0b
librt <geor.polina@gmail.co>
2013-12-12 07:51:14 +0200

    Добавлена поддержка JBehave

--- tests/srvm/lab1/BigNumberInit.java
+++ tests/srvm/lab1/BigNumberInit.java
@@ -1,4 +1,10 @@
 package srvm.lab1;
 
-public class BigNumberInit {
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.steps.Steps;
+
+public class BigNumberInit extends Steps {
 }
