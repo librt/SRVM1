3ddde596dcccfe540fd661a73d8a8533a9c38853
librt <geor.polina@gmail.co>
2013-12-13 19:20:44 +0200

    Реструктуризация

---
+++ src/main/java/srvm/lab1/BigNumber.java
@@ -0,0 +1,31 @@
+package srvm.lab1;
+
+public class BigNumber {
+    long[] numbers;
+    static public final int DEFAULT_SIZE = 256 / (Long.SIZE * 8);
+
+    public BigNumber(int size) {
+        this.numbers = new long[size];
+    }
+
+    public BigNumber(long[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public BigNumber() {
+        this(DEFAULT_SIZE);
+    }
+
+    public BigNumber(long number) {
+        this(number, DEFAULT_SIZE);
+    }
+
+    public BigNumber(long number, int size) {
+        this(size);
+        this.numbers[0] = number;
+    }
+
+    public int size() {
+        return this.numbers.length;
+    }
+}
