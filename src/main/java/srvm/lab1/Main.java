2a795b5a65d37928d456b893c8946a68256f0b8e
librt <geor.polina@gmail.co>
2013-12-26 22:52:45 +0200

    Добавлены побрякушки в функцию Main

--- src/main/java/srvm/lab1/Main.java
+++ src/main/java/srvm/lab1/Main.java
@@ -1,8 +1,29 @@
 package srvm.lab1;
 
+import static srvm.lab1.BinaryArithmetic.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        BigNumber a = new BigNumber(1);
+        System.out.println("1 = " + a.toString());
+        BigNumber b = shiftLeft(a, 1);
+        System.out.println("1 << 1 = " + b.toString());
+        a = shiftRight(b);
+        System.out.println("2 >> 1 = " + a.toString());
+        b = or(a, b);
+        System.out.println("2 | 1 = " + b.toString());
+        BigNumber c = and(a, b);
+        System.out.println("1 & 3 = " + c.toString());
+        b = new BigNumber(2);
+        System.out.println("2 = " + b.toString());
+        a = xor(c, b);
+        System.out.println("1 ^ 2 = " + a.toString());
+        c = xor(a, b);
+        System.out.println("3 ^ 2 = " + c.toString());
+        a = shiftLeft(c, Long.SIZE);
+        System.out.println("1 << " + Long.SIZE + " = " + a.toString());
+        b = or(a, c);
+        System.out.println("10000000000000000 | 1 = " + b.toString());
     }
 }
